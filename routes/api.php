7928cb28843a0b1f482d687a4fcf7c87312c1471
Kamino <iden1107@icloud.com>
2022-02-27 19:50:30 +0900

    web

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Auth;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return Auth::user();
 });
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/register', [LoginController::class, 'register']);
