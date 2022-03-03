4f4c71eec4c842742389d42a097ca3305866fdc5
Kamino <iden1107@icloud.com>
2022-03-04 00:21:43 +0900

    master

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,20 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::post('/register', [LoginController::class, 'register']);
 
 
+// Route::get('/tags/{tag_id}', function($tag_id){
+//     $user = Auth::user();
+//     $tags = Tag::where('user_id', $user->id)->get();
+//     if($tag_id === 'all'){
+//         $tasks = Tag::select('tasks.*', 'tags.title as tags_title','tags.id as tags_id')->leftJoin('tasks','tags.id','=','tasks.tag_id')->where('tags.user_id',$user->id)->get();
+//     }else{
+//         $tasks = Tag::select('tasks.*','tags.title as tags_title', 'tags.id as tags_id')->leftJoin('tasks', 'tags.id','=','tasks.tag_id')->where('tags.user_id', $user->id)->where('tags.id', $tag_id)->get();
+//     }
+//     return compact('tags','tasks');
+// });
+
 Route::get('/tags/{tag_id}', function($tag_id){
-    // $user = Auth::user();
-    $tags = Tag::where('user_id', 4)->get();
+    $user = Auth::user();
+    $tags = Tag::where('user_id', $user->id)->get();
     if($tag_id === 'all'){
         $tasks = Tag::select('tasks.*', 'tags.title as tags_title','tags.id as tags_id')->leftJoin('tasks','tags.id','=','tasks.tag_id')->where('tags.user_id',$user->id)->get();
     }else{
