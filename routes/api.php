1d63dceb0ac87fdd604f8ec3c8ff6a6068b887b1
Kamino <iden1107@icloud.com>
2022-03-04 19:22:19 +0900

    master

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::get('/tags/{tag_id}', function($tag_id){
     }else{
         $tasks = Tag::select('tasks.*','tags.title as tags_title', 'tags.id as tags_id')->leftJoin('tasks', 'tags.id','=','tasks.tag_id')->where('tags.user_id', 4)->where('tags.id', $tag_id)->get();
     }
-    return compact('tags','tasks','user');
+    return compact('tags','tasks');
 });
 
 
