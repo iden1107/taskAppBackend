784c75cf9f8b34ca8db6467e31b316f135b0dd87
Kamino <iden1107@icloud.com>
2022-03-04 22:27:35 +0900

    master

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,8 @@ Route::post('/register', [LoginController::class, 'register']);
 
 // !!!! $userがnullになってしまうのでうまくいかない
 Route::get('/tags/{tag_id}', function($tag_id){
-    $user = Auth::user();
+    // $user = Auth::user();
+    $user = auth()->user();
     $tags = Tag::where('user_id', $user->id)->get();
     if($tag_id === 'all'){
         $tasks = Tag::select('tasks.*', 'tags.title as tags_title','tags.id as tags_id')->leftJoin('tasks','tags.id','=','tasks.tag_id')->where('tags.user_id',$user->id)->get();
