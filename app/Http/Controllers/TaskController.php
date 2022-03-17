3b9eab526d696e3294dca889b6425739e5778e7f
Kamino <iden1107@icloud.com>
2022-03-17 22:54:09 +0900

    heroku対応

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,26 +15,26 @@ class TaskController extends Controller
 {
     public function show($tag_id){
         $user = Auth::user();
-        $tags = Tag::where('user_id', $user->id)->where('status', 1)->get();
-        $tag = Tag::where('user_id', $user->id)->where('status', 1)->where('id',$tag_id)->get();
-        if($tag_id === 'all'){
-            $tasks = Task::select('tasks.*', 'tags.title as tags_title','tags.id as tags_id',)
-            ->leftJoin('tags','tasks.tag_id','=','tags.id')
-            ->where('tags.user_id',$user->id)
-            ->where('tasks.status',1)
-            ->get();
-        }else{
-            $tasks = Task::select('tasks.*','tags.title as tags_title', 'tags.id as tags_id')
-            ->leftJoin('tags', 'tasks.tag_id', '=', 'tags.id')
-            ->where('tags.user_id', $user->id)
-            ->where('tags.id', $tag_id)
-            ->where('tasks.status', 1)
-            ->get();
-        }
+        // $tags = Tag::where('user_id', $user->id)->where('status', 1)->get();
+        // $tag = Tag::where('user_id', $user->id)->where('status', 1)->where('id',$tag_id)->get();
+        // if($tag_id === 'all'){
+        //     $tasks = Task::select('tasks.*', 'tags.title as tags_title','tags.id as tags_id',)
+        //     ->leftJoin('tags','tasks.tag_id','=','tags.id')
+        //     ->where('tags.user_id',$user->id)
+        //     ->where('tasks.status',1)
+        //     ->get();
+        // }else{
+        //     $tasks = Task::select('tasks.*','tags.title as tags_title', 'tags.id as tags_id')
+        //     ->leftJoin('tags', 'tasks.tag_id', '=', 'tags.id')
+        //     ->where('tags.user_id', $user->id)
+        //     ->where('tags.id', $tag_id)
+        //     ->where('tasks.status', 1)
+        //     ->get();
+        // }
         $tag = 'hoge';
         $tags = Tag::all();
         $tasks = Task::all();
-        
+
         return compact('tags','tasks','tag','user');
     }
 
